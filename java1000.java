72e171ff6896722fa37aec7657c4b508d1181c59
leesh031226 <leesh031226@naver.com>
2023-05-18 17:57:00 +0900

    c

---
+++ java1000.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; //
+public class java1000 {
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        int A1 = k.nextInt();
+        int B1 = k.nextInt();
+        System.out.printf("%d", A1 +B1);
+
+
+
+    }
+}
+
