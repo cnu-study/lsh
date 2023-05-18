155dd99104549f0228d402ef411a8856ef4952fc
leesh031226 <leesh031226@naver.com>
2023-05-18 23:53:21 +0900

    c

---
+++ java11654.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class java11654 {
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        int a = k.next().charAt(0);
+        System.out.println(a);
+    }
+}
