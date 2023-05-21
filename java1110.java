e8b24f33378e7fae13ec6a184d2bbf0a3b72c2f4
leesh031226 <leesh031226@naver.com>
2023-05-22 07:40:43 +0900

    c

--- java1110.java
+++ java1110.java
@@ -4,35 +4,32 @@ public class java1110 {
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
         int num1 = k.nextInt();
-        int cnt =0;
-        if (num1/10==0){
-            System.out.println(1);
-        }
-        else{
-            int newnum1 = num1;
-            while(true){
-                int fir = num1/10;
-                int sec = num1-fir*10;
-                int mid = fir+sec;
-                if(mid>9){
-                    cnt++;
-                    mid=mid%10;
-                    num1 = sec*10+mid;
-                    if(newnum1==num1){
-                        break;
-                    }
-                }
-                else{
-                    cnt++;
-                    num1 = sec*10+mid;
-                    if(newnum1==num1){
-                        break;
-                    }
+        int cnt = 0;
 
+
+        int newnum1 = num1;
+        while (true) {
+            int fir = num1 / 10;
+            int sec = num1 - fir * 10;
+            int mid = fir + sec;
+            if (mid > 9) {
+                cnt++;
+                mid = mid % 10;
+                num1 = sec * 10 + mid;
+                if (newnum1 == num1) {
+                    break;
+                }
+            } else {
+                cnt++;
+                num1 = sec * 10 + mid;
+                if (newnum1 == num1) {
+                    break;
                 }
 
             }
+
         }
+
         System.out.println(cnt);
 
     }
