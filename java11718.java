cfdb32cca3c0600cad45768517b975a0efa48967
leesh031226 <leesh031226@naver.com>
2023-05-21 14:52:37 +0900

    c

---
+++ java11718.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class java11718 {
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        while (k.hasNext()){
+            String str = k.nextLine();
+            System.out.println(str);
+        }
+    }
+}
