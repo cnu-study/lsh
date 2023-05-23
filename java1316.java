f5eaae3c3d6a1278b4fdbc280285ba3222d3abd4
leesh031226 <leesh031226@naver.com>
2023-05-23 17:31:03 +0900

    c

--- java1316.java
+++ java1316.java
@@ -1,28 +1,27 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class java1316 {
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
         int num1 = k.nextInt();
-        char[] arr = new char[26];
-        int cnt =0;
-        ArrayList al = new ArrayList();
+        int cnt = num1; //단어 전체가 그룹단어라고 가정
+
         for (int i = 0; i < num1; i++) {
-            String word = k.next();
-            for (int j = 0; j < word.length(); j++) {
-                al.add(word.charAt(j));
+            String str = k.next();
+            boolean test[]= new boolean[26];//기본값은 True
+
+            for (int j = 0; j < str.length()-1; j++) {
+                if(str.charAt(i)!=str.charAt(i+1)){
+                    if(test[str.charAt(i+1)-97]==true){
+                        cnt--;
+                        break;
+                    }
+                }
+                test[str.charAt(i)-97]=true;
             }
-            for (int j = 0; j < word.length(); j++) {
-
-            }
-
-
-
-
-
         }
+        System.out.println(cnt);
 
     }
 }
--- java2577.java
+++ java2577.java
@@ -7,15 +7,29 @@ public class java2577 {
         int A = k.nextInt();
         int B = k.nextInt();
         int C = k.nextInt();
-
         int mul = A*B*C;
-        int[] arr = new int[10];
+        // int mul = (k.nextInt()*k.nextInt()*k.nextInt());
+        //이렇게 간단하게 한 줄로 할 수 있음
+
         String str = Integer.toString(mul);
+
+        int[] arr = new int[10];
         for (int i = 0; i < str.length(); i++) {
             arr[Integer.parseInt(String.valueOf(str.charAt(i)))]++;
         }
         for (int i = 0; i < 10; i++) {
             System.out.println(arr[i]);
         }
+
+//        for (int i = 0; i < 10; i++) { //배열대신
+//            int cnt = 0;
+//            for (int j = 0; j < str.length(); j++) {
+//                if((str.charAt(j)-'0')==i){  문자에 해당하는 아스키 코드
+//                    cnt++; //문자열 다 돌려봐서 i랑 같은 값있느면 +1 (i는1부터 순서대로 )
+//                }
+//            }
+//            System.out.println(cnt);
+//        }
+        //0==48임 str.charAt()에 해당하는게 3(=51)이면 51-48=3
     }
 }
